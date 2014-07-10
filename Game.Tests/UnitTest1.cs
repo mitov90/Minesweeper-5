197544303a6147d2655e306da720afe39c7c4fe8
dbrezoev <d.brezoev@gmail.com>
2014-07-10 23:15:07 +0300

    UnitTesting started; First test for PrepareMatrix method added

---
+++ Game.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿namespace GameTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PrepareMatrixTest()
+        {
+            Field[,] expected =
+            {
+                {new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field() }, 
+                {new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field() },
+                {new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field() },
+                {new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field() },
+                {new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field(), new Field() },
+            };
+
+            Field[,] matrixInitialized = Board.PrepareMatrix(5, 10);
+
+        }
+    }
+}
