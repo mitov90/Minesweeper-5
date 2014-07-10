faf0e1c3c5b093548ff94395ec06f5178a56fcbf
dbrezoev <d.brezoev@gmail.com>
2014-07-10 22:50:16 +0300

    Made exception handling in class Player

--- Player.cs
+++ Player.cs
@@ -10,18 +10,44 @@
 
         public Player(string name, int score)
         {
-            this.name = name;
-            this.score = score;
+            this.Name = name;
+            this.Score = score;
         }
 
         public string Name
         {
-            get { return this.name; }
+            get 
+            { 
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Invalid name of the player.");
+                }
+
+                this.name = value;
+            }
         }
 
         public int Score
         {
-            get { return this.score; }
+            get 
+            { 
+                return this.score;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid score of the player. Score must be positive number.");
+                }
+
+                this.score = value;
+            }
         }
 
         public int CompareTo(object obj)
