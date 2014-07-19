5a5733c478e35f355d0ada17ab66192fe5d46348
Yordan Bankov <bankoff@mail.bg>
2014-07-19 11:35:24 +0300

    Scoreboard Read Write

---
+++ ScoreboardReadWrite.cs
@@ -0,0 +1,32 @@
+﻿namespace Minesweeper
+{
+    using System.Collections.Generic;
+
+    using Minesweeper.Data;
+    using Minesweeper.ReadWrite;
+
+    public class ScoreboardReadWrite
+    {
+        internal readonly GameData gameData;
+
+        public void SaveScoreboard(List<Player> players)
+        {
+            FileReadWrite.Serialize(players, @"..\..\Scoreboard.bin");
+        }
+
+        public void ReadScoreboard()
+        {
+            var players = new List<Player>();
+
+            if (null != (List<Player>)FileReadWrite.Deserialize(@"..\..\Scoreboard.bin"))
+            {
+                players = (List<Player>)FileReadWrite.Deserialize(@"..\..\Scoreboard.bin");
+
+                for (int i = 0; i < players.Count; i++)
+                {
+                    this.gameData.Highscore.AddTopPlayer(players[i]);
+                }
+            }
+        }
+    }
+}
