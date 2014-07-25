c3b7108875427e51be15dd8e7ca2b7d5d3074111
Yordan Bankov <bankoff@mail.bg>
2014-07-25 10:34:57 +0300

    Add FileReadWriteTest

---
+++ Game.Tests/ReadWriteTests/FileReadWriteTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Data;
+using Minesweeper.ReadWrite;
+
+namespace Game.Tests.ReadWriteTests
+{
+    [TestClass]
+    public class FileReadWriteTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FieldAccessException))]
+        public void SerializeNotSerializablePlayer()
+        {
+            Player notSerializablePlayer = new Player("Koki", 4);
+
+            FileReadWrite.Serialize(notSerializablePlayer, @"..\..\NotSerializablePlayer.bin");
+        }
+
+        [TestMethod]
+        public void SerializeSerializablePlayer()
+        {
+            SerializablePlayer actualPlayer = new SerializablePlayer("Emil", 2);
+            FileReadWrite.Serialize(actualPlayer, @"..\..\actualPlayer.bin");
+            SerializablePlayer expectedPlayer = (SerializablePlayer)FileReadWrite.Deserialize(@"..\..\expectedPlayer.bin");
+
+            Assert.AreEqual(expectedPlayer.Name, actualPlayer.Name);
+            Assert.AreEqual(expectedPlayer.Score, actualPlayer.Score);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FieldAccessException))]
+        public void DeserializeNotSerializablePlayer()
+        {
+            FileReadWrite.Deserialize(@"..\..\NotSerializablePlayer.bin");
+        }
+    }
+}
