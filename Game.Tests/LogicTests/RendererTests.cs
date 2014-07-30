ca4ce5b428b2a9e793567835a137027e97b740d4
Yordan Bankov <bankoff@mail.bg>
2014-07-30 13:22:01 +0300

    Add renderer tests

---
+++ Game.Tests/LogicTests/RendererTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using Minesweeper.Data;
+using Minesweeper.Interfaces;
+
+namespace Game.Tests.LogicTests
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper.Logic;
+
+    [TestClass]
+    public class RendererTests
+    {
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            StreamWriter standardOut =
+                new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            Console.SetOut(standardOut);
+        }
+
+        [TestMethod]
+        public void TestWrite()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(string.Format("Pesho" + Environment.NewLine)))
+                {
+                    Console.SetIn(sr);
+
+                    Renderer ab = new Renderer();
+                    ab.Write("Pesho");
+
+                    string expected = string.Format("Pesho" + Environment.NewLine);
+                    Assert.AreEqual<string>(expected, sw.ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestPrintTopPlayers()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(string.Format("Scoreboard" + Environment.NewLine + "1. Ivaylo --> 10" + Environment.NewLine)))
+                {
+                    Console.SetIn(sr);
+
+                    Renderer ab = new Renderer();
+                    Player ivo = new Player("Ivaylo",10);
+                    List<IPlayer> players = new List<IPlayer>();
+                    players.Add(ivo);
+                    ab.PrintTopPlayers(players);
+
+                    string expected = string.Format("Scoreboard" + Environment.NewLine + "1. Ivaylo --> 10" + Environment.NewLine);
+                    Assert.AreEqual<string>(expected, sw.ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestPrintIfNotTopPlayers()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(string.Format("There is still no TOP players!" + Environment.NewLine)))
+                {
+                    Console.SetIn(sr);
+
+                    Renderer ab = new Renderer();
+                    List<IPlayer> players = new List<IPlayer>();
+                    ab.PrintTopPlayers(players);
+
+                    string expected = string.Format("There is still no TOP players!" + Environment.NewLine);
+                    Assert.AreEqual<string>(expected, sw.ToString());
+                }
+            }
+        }
+    }
+}
